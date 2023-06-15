8a98deaa720e02ffd04e8a352489336fe5c28694
omkar vyawhare <136335051+omkarBrainWave@users.noreply.github.com>
2023-06-15 11:45:37 +0530

    Revert "Master"

--- src/main/java/Spring/CiCdGenkinsApplication.java
+++ src/main/java/Spring/CiCdGenkinsApplication.java
@@ -8,7 +8,6 @@ public class CiCdGenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdGenkinsApplication.class, args);
-		System.out.println("Running Success  ");
 	}
 
 }
